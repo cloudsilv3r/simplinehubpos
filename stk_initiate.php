4f2ea5ee0213f8645d015c774fd88571557ab653
SIMPLINE HUB <silverclouddevelopers4@gmail.com>
2021-12-31 20:00:54 +0300

    seventh commit

--- stk_initiate.php
+++ stk_initiate.php
@@ -24,7 +24,7 @@ if(isset($_POST['submit'])){
   */
   
    $PartyA = $_POST['phone']; // This is your phone number, 
-  $AccountReference = '2255';
+  $AccountReference = '2255simplinehub';
   $TransactionDesc = 'Test Payment';
   $Amount = $_POST['amount'];;
  
